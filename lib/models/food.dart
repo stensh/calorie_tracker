8d0943e2c7b2bb9fce9a70e8211754f629852458
sten <t.vuffray@gmail.com>
2026-03-13 11:59:23 +0100

    Ajout de la classe Food

---
+++ lib/models/food.dart
@@ -0,0 +1,17 @@
+class Food {
+  final String name;
+  final int calories;
+  final double protein;
+  final double carbs;
+  final double fat;
+  final double quantity; // measured in g
+
+  Food({
+    required this.name,
+    required this.calories,
+    required this.protein,
+    required this.carbs,
+    required this.fat,
+    required this.quantity,
+  });
+}
